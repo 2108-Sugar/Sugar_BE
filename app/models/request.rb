8f0394349cd5e07c36a623af7ac5cd9ee1a443bf
Didi Dodson <mdodson55@gmail.com>
2022-02-05 08:19:57 -0700

    Add optional to lender

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,6 @@
 class Request < ApplicationRecord
   belongs_to :requested_by, class_name: 'User'
-  belongs_to :lender, class_name: 'User'
+  belongs_to :lender, class_name: 'User', optional: true
 
   belongs_to :community
 end
