fdfde42c25d2697cbbf19a328eeb655282130465
Billy Frey <bfrey16@gmail.com>
2022-02-08 11:33:48 -0700

    bundle update 4

--- script/AfterInstall.sh
+++ script/AfterInstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /var/www/html
-gem install bundler
-bundle install
-bundle update
+#gem install bundler
+#bundle install
+#bundle update
 #RAILS_ENV=production bundle install --path vendor/bundle
-rake db:migrate
+#rake db:migrate
 #RAILS_ENV=production bundle exec rake db:migrate
 #RAILS_ENV=production bundle exec rake assets:clobber
 #RAILS_ENV=production bundle exec rake assets:precompile
